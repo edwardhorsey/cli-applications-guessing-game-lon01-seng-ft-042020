4fd61b67b5da9a179cb620be9f07a0bee97c9d96
Edward Horsey <edwardhorsey@me.com>
2020-03-07 14:48:30 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -39,6 +39,6 @@ if user_guess == "exit"
     return exit_seq
 end
 the_result = check_result(computer, user_guess)
-return game_result_message(the_result, computer)
+game_result_message(the_result, computer)
 end
 
